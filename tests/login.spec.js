bcb34655c79e8e7f197e37523ed87e0c63e37a86
Quan TM <tranmeyer@gmail.com>
2024-11-21 15:52:07 +0100

    2e commit

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -8,6 +8,7 @@ test('Login', async({page})=> {
     const Login = new LoginPage(page)
     await Login.gotologinPage('https://the-internet.herokuapp.com/login')
     await Login.login('tomsmith', 'SuperSecretPassword!')
+	console.log('test')
 
    
 })
