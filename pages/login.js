32966d030356b86874f2df34b84d4ee831a74bc1
Quan TM <tranmeyer@gmail.com>
2024-11-21 15:45:05 +0100

    1st commit

---
+++ pages/login.js
@@ -0,0 +1,16 @@
+exports.LoginPage = class LoginPage {
+    constructor(page){
+        this.page= page 
+        this.usernamebox  = page.locator('id=username')
+        this.passwordbox  = page.locator('id=password')
+        this.loginbutton = page.getByRole('button', { name: 'Login' })
+    }
+    async login(username, password){
+        await this.usernamebox.fill(username)
+        await this.passwordbox.fill(password)
+        await this.loginbutton.click()
+    }
+    async gotologinPage(url){
+        await this.page.goto(url)
+    }
+}
---
+++ tests/login.spec.js
@@ -0,0 +1,13 @@
+const {test, expect} = require('@playwright/test')
+import {LoginPage}   from '../pages/login'
+
+
+test('Login', async({page})=> {
+    // await page.goto('https://the-internet.herokuapp.com/login')
+
+    const Login = new LoginPage(page)
+    await Login.gotologinPage('https://the-internet.herokuapp.com/login')
+    await Login.login('tomsmith', 'SuperSecretPassword!')
+
+   
+})
