8d476aa6de542f7ec4f12cc978de3b9900929405
QuanTM <47085513+qtranmeyer@users.noreply.github.com>
2024-11-21 11:40:40 +0100

    Delete Playwright2 directory

--- Playwright2/tests/3snapshot.spec.js
+++
@@ -1,7 +0,0 @@
-const{test, expect} =require('@playwright/test'); 
-
-
-test('Snapshot', async({page})=>{
-    await page.goto('https://playwright.dev/');
-    expect(await page.screenshot()).toMatchSnapshot('button.png'); 
-});
